997d571222e46312d3895c3c961e996be7329539
Yash <mitroz.yashc@gmail.com>
2026-01-18 17:39:54 +0530

    feat: Initialize a basic Vite React frontend application with core files and simplified dependencies.

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import "./index.css";
-import { BrowserRouter } from "react-router-dom";
-import { AuthContextProvider } from "./context/AuthContext.jsx";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
 
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-	<React.StrictMode>
-		<BrowserRouter>
-			<AuthContextProvider>
-				
-					<App />
-				
-			</AuthContextProvider>
-		</BrowserRouter>
-	</React.StrictMode>
-);
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
